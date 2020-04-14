ce94faa01d6c567457b40fff5778f6f76f8150fd
Rob Anderson <rob@andrsn.uk>
2020-04-14 14:19:44 +0100

    feat: improve push-clasp to take the script-id
    also remove old sheet script

--- scripts/push-clasp.sh
+++ scripts/push-clasp.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
+
+echo -n "Script ID: "
+read SCRIPT_ID
+
+if [ -z "$SCRIPT_ID" ]
+then
+  echo "SCRIPT_ID is required"
+  exit 1
+fi
+
+
 
 # Get the directories to run scripts in
 DIRS=`ls -d google-scripts/*/`
@@ -21,7 +32,11 @@ do
   # Temporarily move to the directory and push it
   #
   pushd $DIR > /dev/null
+  echo '{"scriptId":"'$SCRIPT_ID'"}' > .clasp.json
   npx clasp push
   popd > /dev/null
   echo
 done
+
+echo "Make sure to set 'CATALYST_HOOK_TOKEN' and 'CATALYST_HOOK_URL' on your project and setup triggers"
+echo "e.g. cd google-scripts/eoi-form; npx clasp open"
